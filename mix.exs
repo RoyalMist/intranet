1c127658428527281cfcda868171c62e76ed2bf9
RoyalMist <thibault.fouache@gmail.com>
2022-01-17 23:26:55 +0100

    Libcluster

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule Intranet.MixProject do
       {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:mjml, "~> 1.1"},
+      {:libcluster, "~> 3.3"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
